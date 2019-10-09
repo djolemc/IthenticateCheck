e6ab35db5d82b95f8b5cc948a18db2d7d0a51aa2
Dragoljub Djordjevic <dragoljubd@gmail.com>
2019-10-09 15:53:38 +0200

    Popravljena funkcija za promenu dokumenta

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 /**
-*
+ *
  **/
 
 
@@ -17,11 +17,9 @@ function myFunction(obj) {
     if (ext == 'pdf') {
         //document.getElementById('frame').innerHTML = `<iframe id="fred" style="border:1px solid #666CCC" title="PDF in an i-Frame" src="../assets/files/${link}" frameborder="1" scrolling="auto" width="50%" ></iframe>`;
         document.getElementById('frame').innerHTML = '<iframe title="PDF in an i-Frame" src="../assets/files/' + link + '" frameborder="1" scrolling="auto"  ></iframe>';
-    }
-    else if (ext == 'docx' || ext == 'doc') {
+    } else if (ext == 'docx' || ext == 'doc') {
         document.getElementById('frame').innerHTML = '<iframe src="https://view.officeapps.live.com/op/embed.aspx?src=' + link + '" frameborder="0">This is an embedded <a target="_blank" href="http://office.com">Microsoft Office</a> document, powered by <a target="_blank" href="http://office.com/webapps">Office Online</a>.</iframe>';
-    }
-    else {
+    } else {
         document.getElementById('frame').innerHTML = "Kliknite na link da pogledate fajl.";
     }
     return false;
@@ -33,9 +31,6 @@ if (window.location.pathname.split("/").pop() === 'index.php') {
         $("#spinner").append("<img class='spiner' src='../assets/images/loading.gif'>");
 
 
-
-
-
         $.ajax({
             url: "index.php",
             type: "post",
@@ -43,109 +38,97 @@ if (window.location.pathname.split("/").pop() === 'index.php') {
             success: function (response) {
                 $("#spinner").hide();
                 setTimeout(function () {
-                var data = JSON.parse(response);
-                console.log(data);
-                var trHTML = '';
-                $.each(data, function (i, item) {
+                    var data = JSON.parse(response);
+                    console.log(data);
+                    var trHTML = '';
+                    $.each(data, function (i, item) {
 
-                    files_array = item.filenames;
-                    trHTML += '<div class="form[checked' + i + ']  single_row">';
-                    trHTML += '<div class="w3">';
-                    trHTML += ' <span  class="form[checked' + i + '] remove_button"  data-toggle="tooltip" title="Remove file"  name="form[checked<?php echo $i?>][]" onclick="deleteX(this); countDivs();">X</span><br>';
-                    trHTML += '<span class="form[checked' + i + '] number">' + " " + (i+1) + ". " + '</span>';
-                    trHTML += '</div>';
+                        files_array = item.filenames;
+                        trHTML += '<div class="form[checked' + i + ']  single_row">';
+                        trHTML += '<div class="w3">';
+                        trHTML += ' <span  class="form[checked' + i + '] remove_button"  data-toggle="tooltip" title="Remove file"  name="form[checked<?php echo $i?>][]" onclick="deleteX(this); countDivs();">X</span><br>';
+                        trHTML += '<span class="form[checked' + i + '] number">' + " " + (i + 1) + ". " + '</span>';
+                        trHTML += '</div>';
 
-                    trHTML += '<div class="w25">';
+                        trHTML += '<div class="w25">';
 
-                    trHTML += '<span id="autori">Prezime i ime autora:';
-                    trHTML += '<input class="form[checked' + i + '] input-res" type="text" name="form[checked' + i + '][]" value="' + item.author_last + '"><br>';
-                    trHTML += '<input class="form[checked' + i + ']" type="text" name="form[checked' + i + '][]" value="' + item.author_first + '">';
-                    trHTML += '</span></div>';
+                        trHTML += '<span id="autori">Prezime i ime autora:';
+                        trHTML += '<input class="form[checked' + i + '] input-res" type="text" name="form[checked' + i + '][]" value="' + item.author_last + '"><br>';
+                        trHTML += '<input class="form[checked' + i + ']" type="text" name="form[checked' + i + '][]" value="' + item.author_first + '">';
+                        trHTML += '</span></div>';
 
-                    trHTML += '<div class="w60">';
-                    trHTML += '<input class="form[checked' + i + '] journal_title" type="text" hidden name="form[checked' + i + '][]" value="' + item.journal_title + '">'+'Časopis: '+'<strong>' +item.journal_title +'</strong><span>';
-                    // trHTML += '<span class="form[checked' + i + '] journal_title" type="text" name="form[checked' + i + '][]" value="' + item.journal_title + '">'+'Časopis: '+'<strong>' +item.journal_title +'</strong><span>';
-                    trHTML += '<textarea class="form[checked' + i + '] title" rows="3" type="text" name="form[checked' + i + '][]">' + item.submission_title + "\\" + item.submission_id + "\\" + '</textarea>';
-                    trHTML += '<span id="filename" class="proba">' +'Naziv dokumenta: '+ item.filenames[0] + '</span>';
-                    trHTML += '</div>';
+                        trHTML += '<div class="w60">';
+                        trHTML += '<input class="form[checked' + i + '] journal_title" type="text" hidden name="form[checked' + i + '][]" value="' + item.journal_title + '">' + 'Časopis: ' + '<strong>' + item.journal_title + '</strong><span>';
+                        // trHTML += '<span class="form[checked' + i + '] journal_title" type="text" name="form[checked' + i + '][]" value="' + item.journal_title + '">'+'Časopis: '+'<strong>' +item.journal_title +'</strong><span>';
+                        trHTML += '<textarea class="form[checked' + i + '] title" rows="3" type="text" name="form[checked' + i + '][]">' + item.submission_title + "\\" + item.submission_id + "\\" + '</textarea>';
+                        trHTML += '<span id="filename" class="proba">' + 'Naziv dokumenta: ' + item.filenames[0] + '</span>';
+                        trHTML += '</div>';
+                        trHTML += '<div class="w10">';
+                        trHTML += ' <button id="0" class="form[checked' + i + '] view_button"  href="1.pdf" onclick="myFunction(this);return false">Pogledaj dokument</button><br class="form[checked' + i + ']">';
 
-                    trHTML += '<div class="w10">';
+                        if (files_array.length > 1) {
 
+                            trHTML += '<span class="pf"><i class="fa fa-arrow-left"></i></span>';
+                            trHTML += '<span class="nf"><i class="fa fa-arrow-right"></i></span>';
 
-                    trHTML += ' <button id="0" class="form[checked' + i + '] view_button"  href="1.pdf" onclick="myFunction(this);return false">Pogledaj dokument</button><br class="form[checked' + i + ']">';
-                    if (files_array.length > 1) {
+                        }
+                        trHTML += '<span class="filename"><input class="form[checked' + i + '] file" type="hidden" name="form[checked' + i + '][]" value="' + item.filenames[0] + '"></span>';
+                        trHTML += '</div>';
+                        trHTML += '</div>';
 
-                        trHTML += '<span class="pf"><i class="fa fa-arrow-left"></i></span>';
-                        trHTML += '<span class="nf"><i class="fa fa-arrow-right"></i></span>';
-                    }
-                    trHTML += '</div>';
-                    trHTML += '</div>';
+                        trHTML += '<input class="form[checked' + i + ']" type="hidden" name="form[checked' + i + '][]" value="' + item.journal_issn + '">';
+                        trHTML += '<input class="form[checked' + i + ']" type="hidden" name="form[checked' + i + '][]" value="' + item.ithenticate_id + '">';
 
-
-                    trHTML += '<input class="form[checked' + i + ']" type="hidden" name="form[checked' + i + '][]" value="' + item.journal_issn + '">';
-                    trHTML += '<input class="form[checked' + i + ']" type="hidden" name="form[checked' + i + '][]" value="' + item.ithenticate_id + '">';
-                    trHTML += '<span class="filename"><input class="form[checked' + i + '] file" type="hidden" name="form[checked' + i + '][]" value="' + item.filenames[0] + '"></span>';
-                    trHTML += '<input class="form[checked' + i + ']" type="hidden" name="form[checked' + i + '][]" value="' + item.ithenticatePassword + '">';
-                    trHTML += '<input class="form[checked' + i + ']" type="hidden" name="form[checked' + i + '][]" value="' + item.ithenticateUsername + '">';
-                    trHTML += '<input class="form[checked' + i + ']" type="hidden" name="form[checked' + i + '][]" value="' + item.journal_id + '">';
-                    trHTML += '<input class="form[checked' + i + ']" type="hidden" name="form[checked' + i + '][]" value="' + item.mail_to + '">';
-                    trHTML += '<input class="form[checked' + i + ']" type="hidden" name="form[checked' + i + '][]" value="' + item.path + '">';
-
+                        trHTML += '<input class="form[checked' + i + ']" type="hidden" name="form[checked' + i + '][]" value="' + item.ithenticatePassword + '">';
+                        trHTML += '<input class="form[checked' + i + ']" type="hidden" name="form[checked' + i + '][]" value="' + item.ithenticateUsername + '">';
+                        trHTML += '<input class="form[checked' + i + ']" type="hidden" name="form[checked' + i + '][]" value="' + item.journal_id + '">';
+                        trHTML += '<input class="form[checked' + i + ']" type="hidden" name="form[checked' + i + '][]" value="' + item.mail_to + '">';
+                        trHTML += '<input class="form[checked' + i + ']" type="hidden" name="form[checked' + i + '][]" value="' + item.path + '">';
 
 
+                        trHTML += '</div>';
 
 
-                    trHTML += '</div>';
+                    });
+                    $('#single_submission').append(trHTML);
+                     $("#submit_button").show();
 
+                    i = 1;
+                    length = files_array.length;
 
-                });
-                $('#single_submission').append(trHTML);
-                    $("#submit_button").show();
+                    $('.nf').bind("click", function () {
+                        i = $(this).siblings('button').attr("id");
+                        i++;
+                        if (i == length) {
+                            i = 0
+                        }
+                        $(this).siblings('button').attr("href", (files_array[i]));
+                        $(this).siblings('button').attr("id", (i));
 
-                // i=1;
-                length = files_array.length;
+                         $(this).closest('div').prev().find('.proba').html('Naziv dokumenta: ' + files_array[i]);
+                        $('#file_id').attr("value", (files_array[i]));
+                        $(this).siblings('.filename').children('input').attr('value', files_array[i]);
 
+                        if (i == length) {
+                            i = 0;
+                        }
+                    });
 
-                $('.nf').bind("click", function () {
-
-                    i = $(this).siblings('button').attr("id");
-
-
-                    i++;
-                    if (i == (length + 1)) {
-                        i = 0
-                    }
-                    $(this).siblings('button').attr("href", (files_array[i]));
-                    $(this).siblings('button').attr("id", (i));
-                    $('.proba').html('Ime dokumenta: '+ files_array[i]);
-                    $('#file_id').attr("value", (files_array[i]));
-                    (($(this).siblings('.filename').children('input').attr('value', files_array[i])));
-
-                    if (i > length) {
-                        i = 0
-                    }
-                });
-
-                //o = i;
-                $('.pf').bind("click", function () {
-                    i = $(this).siblings('button').attr("id");
-                    console.log(i);
-                    if (i == 0) {
-                        i = length + 1
-                    }
-                    i--;
-                    $(this).siblings('button').attr("href", (files_array[i]));
-                    $(this).siblings('button').attr("id", (i));
-                    (($(this).siblings('.filename').children('input').attr('value', files_array[i])));
-
-                 //todo popraviti prikaz pojedinacnog imena dokumenta
-                    $('.proba').html('Ime dokumenta: '+ files_array[i]);
-
-                });
-            }, 1000);
+                    $('.pf').bind("click", function () {
+                        i = $(this).siblings('button').attr("id");
+                        if (i == 0) {
+                            i = length;
+                        }
+                        i--;
+                        $(this).siblings('button').attr("href", (files_array[i]));
+                        $(this).siblings('button').attr("id", (i));
+                        $(this).siblings('.filename').children('input').attr('value', files_array[i]);
+                        $(this).closest('div').prev().find('.proba').html('Naziv dokumenta: ' + files_array[i]);
+                    });
+                }, 1000);
             },
             error: function (jqXHR, textStatus, errorThrown) {
-                // console.log(textStatus, errorThrown);
+
             }
 
         });
@@ -159,15 +142,15 @@ if (window.location.pathname.split("/").pop() === 'index.php') {
 //Brise rad iz forme koji ne treba da se salje na proveru
 
 function deleteX(obj) {
-   if (confirm('Da li ste sigurni da želite da obrišete ovaj fajl?')) {
-       className = (obj.className.split(" ")[0]);
-       // console.log(className);
+    if (confirm('Da li ste sigurni da želite da obrišete ovaj fajl?')) {
+        className = (obj.className.split(" ")[0]);
+        // console.log(className);
 
-       var elements = document.getElementsByClassName(className);
-       while (elements.length > 0) {
-           elements[0].parentNode.removeChild(elements[0]);
-       }
-   }
+        var elements = document.getElementsByClassName(className);
+        while (elements.length > 0) {
+            elements[0].parentNode.removeChild(elements[0]);
+        }
+    }
 }
 
 //Prikazuje poruku ako nema radova za slanje na proveru
@@ -192,7 +175,7 @@ $(document).ready(function () {
             data: 'logout',
             success: function (response) {
                 console.log(response);
-               window.location.href = response;
+                window.location.href = response;
             }
         });
     });
